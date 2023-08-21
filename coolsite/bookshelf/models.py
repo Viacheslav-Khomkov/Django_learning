f7c0b0e62d7f499372c05f2668a57305653ff92d
Viacheslav-Khomkov <slavahc71@gmail.com>
2023-08-21 22:06:30 +0300

    Дополнил модели внешними ключами

--- coolsite/bookshelf/models.py
+++ coolsite/bookshelf/models.py
@@ -2,30 +2,36 @@ from django.db import models
 
 
 class Styles(models.Model):  # классы для CSS
-    name = models.CharField(max_length=255)
-    class_name = models.CharField(max_length=255)  # Название класса для оформления
+    name = models.CharField(max_length=100, db_index=True)
+    class_name = models.CharField(max_length=100)  # Название класса для оформления
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Tags(models.Model):  # классы для CSS
+    name = models.CharField(max_length=100, db_index=True)
 
     def __str__(self):
         return f'{self.name}'
 
 
 class Paragraphs(models.Model):  # Абзацы книг
-    line = models.IntegerField()  # номер абзаца в книге
-    book_id = models.IntegerField()  # Ссылка на книгу
-    tag_id = models.IntegerField()  # Ссылка на тэг HTML
-    chapter_id = models.IntegerField()  # индекс главы (для отображения по главам)
+    book = models.ForeignKey('Books', on_delete=models.PROTECT)
+    tag = models.models.ForeignKey('Tags', on_delete=models.PROTECT, null=True)  # Ссылка на тэг HTML
     text = models.TextField(blank=True)  # Текст абзаца
 
+
 class Notes(models.Model):
-    par_id = models.IntegerField()  # абзаца в книге
-    book_id = models.IntegerField()  # Ссылка на книгу
+    par_id = models.ForeignKey('Paragraphs', on_delete=models.PROTECT)  # абзаца в книге
+    book = models.ForeignKey('Books', on_delete=models.PROTECT)
     text = models.TextField(blank=True)  # Текст абзаца
 
 
 class UserDictionary(models.Model):
-    user_id = models.IntegerField()  # Ссылка на пользователя
-    book_id = models.IntegerField()  # Ссылка на книгу - если не задана - настройка общая
-    source = models.CharField(max_length=255)
+    user = models.ForeignKey('Users', on_delete=models.PROTECT)  # Ссылка на пользователя
+    book = models.ForeignKey('Books', on_delete=models.PROTECT)
+    original = models.CharField(max_length=255)
     translate = models.CharField(max_length=255)
 
 
@@ -33,24 +39,38 @@ class Users(models.Model):
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
+    lang = models.ForeignKey('Languages', on_delete=models.PROTECT, null=True)  # Язык пользователя
 
     def __str__(self):
         return f'{self.name}'
 
-class UserProperties(models.Model):  # Настройки внешнего вида страницы для каждого пользователя и каждой книги
-    user_id = models.IntegerField()  # Ссылка на пользователя
-    style_id = models.IntegerField()  # Ссылка на основной стиль пользователя
-    book_id = models.IntegerField(blank=True)  # Ссылка на книгу - если не задана - настройка общая
 
+class UserProperties(models.Model):  # Настройки внешнего вида страницы для каждого пользователя и каждой книги
+    user = models.ForeignKey('Users', on_delete=models.PROTECT)  # Ссылка на пользователя
+    style = models.ForeignKey('Styles', on_delete=models.PROTECT)  # Ссылка на основной стиль пользователя
+    book = models.ForeignKey('Books', on_delete=models.PROTECT, null=True)
+    lang = models.ForeignKey('Languages', on_delete=models.PROTECT, null=True)  # Язык пользователя
+
+
+class Genres(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class BookGenres(models.Model):
+    book = models.ForeignKey('Books', on_delete=models.PROTECT)
+    genre = models.ForeignKey('Genres', on_delete=models.PROTECT)
 
 class Books(models.Model):
-    author_id = models.IntegerField()
+    author = models.ForeignKey('Authors', on_delete=models.PROTECT)
     name = models.CharField(max_length=255)
     content = models.TextField(blank=True)
     photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
     date = models.DateField()
     isbn = models.CharField(max_length=20)
-    lang_id = models.IntegerField()
+    lang = models.ForeignKey('Languages', on_delete=models.PROTECT)
 
     def __str__(self):
         return f'{self.name}'
@@ -58,29 +78,28 @@ class Books(models.Model):
 
 class Authors(models.Model):
     name = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
+    biography = models.TextField(blank=True)
     photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
     date_of_birth = models.DateField()
-    date_of_death = models.DateField()
+    date_of_death = models.DateField(null=True)
     time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
-    country_id = models.IntegerField()
-    lang_id = models.IntegerField()
+    country = models.ForeignKey('Countries', on_delete=models.PROTECT, null=True)
+    lang = models.ForeignKey('Languages', on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return f'{self.name}'
 
 class Countries(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100, db_index=True)
     short_name = models.CharField(max_length=10)
-    lang_id = models.IntegerField()
+    lang = models.ForeignKey('Languages', on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return f'{self.name}'
 
 
 class Languages(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100, db_index=True)
     short_name = models.CharField(max_length=10)
 
     def __str__(self):
