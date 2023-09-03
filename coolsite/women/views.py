b79b867e701ce9a47a63341a0c359aea461f7483
Viacheslav-Khomkov <slavahc71@gmail.com>
2023-09-03 15:07:33 +0300

    Перевел форму на класс ModelForm, добавил свой валидатор

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -36,14 +36,15 @@ def about(request):
 
 def add_page(request):
     if request.method == 'POST':
-        form = AddPostForm(request.POST)
+        form = AddPostForm(request.POST, request.FILES)
         if form.is_valid():
             # print(form.cleaned_data)
-            try:
-                Women.objects.create(**form.cleaned_data)
-                return redirect('home')
-            except:
-                form.add_error(None, 'Ошибка добавления поста')
+            # try:
+                # Women.objects.create(**form.cleaned_data)
+            form.save()
+            return redirect('home')
+            # except:
+            #     form.add_error(None, 'Ошибка добавления поста')
     else:
         form = AddPostForm()
 
