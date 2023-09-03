48fc3fa3af75db3be1d1d16ed4c326c8dcebc84a
Viacheslav-Khomkov <slavahc71@gmail.com>
2023-09-03 14:02:15 +0300

    Использование форм, не связанных с моделями

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 
+from .forms import *
 from .models import *
 
 #
@@ -34,7 +35,20 @@ def about(request):
 
 
 def add_page(request):
-    return HttpResponse(f"<h1>Добавление статьи</h1>")
+    if request.method == 'POST':
+        form = AddPostForm(request.POST)
+        if form.is_valid():
+            # print(form.cleaned_data)
+            try:
+                Women.objects.create(**form.cleaned_data)
+                return redirect('home')
+            except:
+                form.add_error(None, 'Ошибка добавления поста')
+    else:
+        form = AddPostForm()
+
+    return render(request, 'women/addpage.html',
+                  {'form': form, 'title': 'Добавление статьи'})
 
 
 def contact(request):
